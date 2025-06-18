5769887680880ae30a17fac4be32c1c3bb29049c
florykhan <florykhan@gmail.com>
2025-06-17 20:26:10 -0700

    Add basic layout for Skills section

---
+++ src/components/Skills.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Skills() {
+  return (
+    <section>
+      <h2>Skills</h2>
+      <p>Python, JavaScript, C++, React, Git, Machine Learning...</p>
+    </section>
+  );
+}
+
+export default Skills;
