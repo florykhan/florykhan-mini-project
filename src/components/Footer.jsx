b67c80898b975a0ccf439bf5892968d3d5838884
florykhan <florykhan@gmail.com>
2025-06-17 20:51:24 -0700

    Add footer with copyright

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <hr />
+      <p>© 2025 Ilian Khankhalaev. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
