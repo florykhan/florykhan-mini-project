41589013b17b6981bd4bd9f8a23512b1d39a30ea
florykhan <florykhan@gmail.com>
2025-06-17 19:57:15 -0700

    Add Projects Section with initial setup

---
+++ src/components/Projects.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Projects() {
+  return (
+    <section>
+      <h2>Projects</h2>
+      <p>Here I will showcase some of my projects.</p>
+    </section>
+  );
+}
+
+export default Projects;
